8f4b027ed05c2ad3441d317d5bc39b3fde452c16
maducz <160507829+maducz@users.noreply.github.com>
2024-06-28 11:34:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function somar() {
+    let num1=8;
+    let num2=12;
+    let result=num1+num2;
+    document.getElementById("receptor").innerHTML=result;
+}
+
+function sub() {
+    let num1=8;
+    let num2=12;
+    let result=num1-num2;
+    document.getElementById("receptor").innerHTML=result;
+}
+
+function multiplicar() {
+    let num1=8;
+    let num2=12;
+    let result=num1*num2;
+    document.getElementById("receptor").innerHTML=result;
+}
+
+function dividir() {
+    let num1=8;
+    let num2=12;
+    let result=num1/num2;
+    document.getElementById("receptor").innerHTML=result;
+}
